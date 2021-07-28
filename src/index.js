659f6552956b8c1f64b790fba0d98299a1439e6a
andrescondo <1dkevinandres.b@gmail.com>
2021-07-27 21:28:11 -0500

    creacion de estrutura del programa y archivo del backend

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+// const methodOverride = require('method-override');
+// const session = require('express-session');
+// const flash = require('connect-flash');
+// const passport = require('passport');
+
+// Initializations
+const app = express();
+const {mongoose} =  require('./database');
+
+//Settings
+app.set('port', process.env.PORT || 4000);
+
+//Middleware
+app.use(morgan('dev')); //ver las peticiones con sus respectiva descripcion
+app.use(express.json());
+
+//Routes
+app.use('/api/client', require('./routers/users.routes'));
+ 
+//Static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Starting the server
+app.listen(app.get('port'), () => {
+  console.log(`Server on port ${app.get('port')}`);
+});
+
+
