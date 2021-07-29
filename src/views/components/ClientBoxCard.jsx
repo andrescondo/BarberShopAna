c7f80fd579edbaa052e545c32e9652144ab05cdb
andrescondo <1dkevinandres.b@gmail.com>
2021-07-29 16:25:05 -0500

    añadido interactividad para el input de cita

--- src/views/components/ClientBoxCard.jsx
+++ src/views/components/ClientBoxCard.jsx
@@ -4,13 +4,12 @@ import {useEffect, useState} from 'react'
 import { useParams } from 'react-router';
 
 const stateAttention = {
-  attention: ''
+  cita: 'No',
+  a:''
 }
 
 const ClientBoxCard = ({name, date, ci, phone, email}) => {
-  const [attention, setAttention] = useState();
-
-
+  const [attention, setAttention] = useState(stateAttention);
 
   // // console.log(useParams())
   // const {id} = useParams()
@@ -28,6 +27,14 @@ const ClientBoxCard = ({name, date, ci, phone, email}) => {
   //   })
   // }
 
+  const handleChange = (e) => {
+    setAttention({ 
+      ...attention,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  console.log(attention)
 
   return(
     <div className="ClientBoxCrad">
@@ -39,10 +46,18 @@ const ClientBoxCard = ({name, date, ci, phone, email}) => {
       <div>
         <p>¿Cita atendida?</p>
         <p>Cita {date}</p>
-        <select>
+        <select onChange={handleChange} value={attention.cita} name="cita">
           <option value="Si">Si</option>
           <option value="No">No</option>
         </select>
+        {attention.cita === "Si" ? (
+          <label htmlFor="attention">
+            <p>Motivo de la cita</p>
+            <input type="text"placeholder="Motivo de la cita" />
+          </label>
+        ) : ''
+
+        }
       </div>
     </div>
   )
